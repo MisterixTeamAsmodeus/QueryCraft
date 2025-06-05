7c2e40a1faaaf42235ed7520e1347e80271fdc46
a.buzuverov <a.buzuverov@localhost.com>
2025-06-05 16:54:22 +0300

    Обновление инклудов

--- include/QueryCraft/querycraft.h
+++ include/QueryCraft/querycraft.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#include "conditiongroup.h"
+#include "enum/conditionviewtype.h"
+#include "enum/logicaloperator.h"
+#include "operator/ioperator.h"
+#include "sortcolumn.h"
 #include "sqltable.h"
+#include "table.h"
+
+#include <TypeConverterApi/typeconverterapi.h>
